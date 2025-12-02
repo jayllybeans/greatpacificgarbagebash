e2ded5f713c1fe2b2047a8f4180376b0e3db2fc8
jayllybeans <jmvalidzic@email.neit.edu>
2025-12-02 10:18:54 -0500

    Updated to add the new screens

--- code.js
+++ code.js
@@ -1,7 +1,9 @@
 import {GameOverScreen} from "./gameScreens/gameOverScreen.js";
 import {GameScreen} from "./gameScreens/gameScreen.js";
+import {InstructionScreen} from "./gameScreens/instructionScreen.js";
 import {TitleScreen} from "./gameScreens/titleScreen.js";
 import {Toolbox} from "./toolbox.js";
+import {VictoryScreen} from "./gameScreens/victoryScreen.js";
 
 let canvas = document.getElementById("myCanvas");
 let pencil = canvas.getContext("2d");
@@ -9,8 +11,10 @@ let pencil = canvas.getContext("2d");
 let arr = ["a", "b", "c"];
 let toolbox = new Toolbox();
 let title = new TitleScreen(canvas, pencil);
+let instructions = new InstructionScreen(canvas, pencil);
 let game = new GameScreen(canvas, pencil);
 let gameOver = new GameOverScreen(canvas, pencil);
+let victory = new VictoryScreen(canvas, pencil);
 
 let state = title;
 
@@ -32,11 +36,17 @@ function gameLoop(){
     if (command == "title"){
         state = title;
     }
+    if (command == "instructions"){
+        state = instructions;
+    }
     if (command == "game"){
         state = game;
     }
     if (command == "gameOver"){
         state = gameOver;
+    }
+    if (command == "victory"){
+        state = victory;
     }
 }
 
