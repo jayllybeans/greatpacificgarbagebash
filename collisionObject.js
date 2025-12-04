814a8affb238336e1f8563d4d4b73280d99e47e6
jayllybeans <jmvalidzic@email.neit.edu>
2025-12-04 09:11:00 -0500

    Made some changes

--- collisionObject.js
+++ collisionObject.js
@@ -1,4 +1,32 @@
 export class collisionObject{
     canvas;
     pencil;
+    sprite;
+    x = 1890;
+    y = Math.random() * (150 - 50) + 50;
+    height = 50;
+    width = 50;
+
+    topLeftCorner;
+    bottomRightCorner;
+
+    constructor(canvas, pencil, sprite){
+        this.pencil = pencil;
+        this.canvas = canvas;
+        this.sprite = sprite;
+    }
+
+    draw(){
+         this.topLeftCorner = { 
+            x : this.x,
+            y : this.y - this.height
+        }
+
+        this.bottomRightCorner = { 
+            x : this.x + this.width,
+            y : this.y - this.height + this.height
+        }
+
+        this.pencil.drawImage(this.sprite, this.x, this.y - this.height, this.width, this.height);
+    }
 }
--- gameScreens/gameScreen.js
+++ gameScreens/gameScreen.js
@@ -1,4 +1,4 @@
-import {Toolbox} from "./toolbox.js";
+import {Toolbox} from "../toolbox.js";
 import {collisionObject} from "../collisionObject.js";
 
 export class GameScreen{
@@ -8,16 +8,44 @@ export class GameScreen{
     quota = 0;
     dailyQuota = Math.floor(Math.random() * (20 - 10)) + 10;
     toolbox = new Toolbox();
+    appleCore = document.getElementById("appleCore");
+    plasticBag = document.getElementById("plasticBag");
+    tire = document.getElementById("tire");
+    trash = [new collisionObject(this.canvas, this.pencil, this.appleCore), new collisionObject(this.canvas, this.pencil, this.plasticBag), (this.canvas, this.pencil, this.tire)];
+    cuttlefish = document.getElementById("cuttlefish");
+    shark = document.getElementById("shark");
+    starfish = document.getElementById("starfish");
+    seaCreatures = [new collisionObject(this.canvas, this.pencil, this.cuttlefish), new collisionObject(this.canvas, this.pencil, this.shark), new collisionObject(this.canvas, this.pencil, this.starfish)];
+    aqua = document.getElementById("aqua");
+    currentObject = this.shark;
+    arrayChoice = Math.floor(Math.random() * (2 - 1)) + 1;
+    wasHit;
 
     constructor(canvas, pencil){
         this.canvas = canvas;
         this.pencil = pencil;
-
-        this.onClick = this.onClick.bind(this);
     }
 
-    onClick(){
+    quotaCheck(quota){
         this.changeScreen = true;
+        if (quota < 0){
+            return "gameOver";
+        }
+        else{
+            return "victory";
+        }
+    }
+
+    isCollision(collisionObject){
+        let isFarEnoughRight = this.x > collisionObject.topLeftCorner.x;
+        let isLowEnough = this.y > collisionObject.topLeftCorner.y;
+        let isFarEnoughLeft = this.x < collisionObject.bottomRightCorner.x;
+        let isHighEnough = this.y < collisionObject.bottomRightCorner.y;
+
+         if((isFarEnoughRight && isLowEnough && isFarEnoughLeft && isHighEnough)){
+            return true;
+         }
+         return false;
     }
 
     enter() {
@@ -33,6 +61,28 @@ export class GameScreen{
         this.pencil.font = "50px Georgia"
         this.pencil.fillText("Quota: " + this.quota + "/" + this.dailyQuota, 10, 50);
 
-        //return "gameOver";
+        this.pencil.drawImage(this.aqua, this.x, this.y, 150, 50);
+
+        this.pencil.drawImage(this.currentObject, this.currentObject.x, this.currentObject.y, this.currentObject.height, this.currentObject.width);
+
+        this.wasHit = this.isCollision(this.currentObject);
+
+        if(this.wasHit){
+            for(let i = 0; i < this.trash.length; i++){
+                if (this.currentObject == this.trash[i]){
+                    quota++;
+                }
+            }
+            
+            for(let i = 0; i <this.seaCreatures.length; i++){
+                if (this.currentObject == this.seaCreatures[i]){
+                    quota--;
+                }
+            }
+        }
+
+        if(quota < 0 || quota == dailyQuota){
+            this.quotaCheck(quota);
+        }
     }
 }
